ba9e59c01555a3de062490ebf4b34fcf8ebbc1f0
Noor Saeed <124183389+611noorsaeed@users.noreply.github.com>
2023-03-12 11:08:08 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import streamlit as st
+
+# load data
+data = pd.read_csv('creditcard.csv')
+
+# separate legitimate and fraudulent transactions
+legit = data[data.Class == 0]
+fraud = data[data.Class == 1]
+
+# undersample legitimate transactions to balance the classes
+legit_sample = legit.sample(n=len(fraud), random_state=2)
+data = pd.concat([legit_sample, fraud], axis=0)
+
+# split data into training and testing sets
+X = data.drop(columns="Class", axis=1)
+y = data["Class"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=2)
+
+# train logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# evaluate model performance
+train_acc = accuracy_score(model.predict(X_train), y_train)
+test_acc = accuracy_score(model.predict(X_test), y_test)
+
+# create Streamlit app
+st.title("Credit Card Fraud Detection Model")
+st.write("Enter the following features to check if the transaction is legitimate or fraudulent:")
+
+# create input fields for user to enter feature values
+input_df = st.text_input('Input All features')
+input_df_lst = input_df.split(',')
+# create a button to submit input and get prediction
+submit = st.button("Submit")
+
+if submit:
+    # get input feature values
+    features = np.array(input_df_lst, dtype=np.float64)
+    # make prediction
+    prediction = model.predict(features.reshape(1,-1))
+    # display result
+    if prediction[0] == 0:
+        st.write("Legitimate transaction")
+    else:
+        st.write("Fraudulent transaction")
